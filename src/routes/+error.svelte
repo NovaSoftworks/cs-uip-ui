8eeb9fc063b6e4a0f6b19112c36ab63b33613133
Marc LAMY <29371248+marclamy@users.noreply.github.com>
2025-07-05 12:39:45 +0200

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import "../app.css";
+
+    import { page } from "$app/state";
+</script>
+
+<div class="min-h-screen bg-light flex items-center justify-center">
+    <div class="text-center">
+        <h1 class="text-9xl font-bold text-medium">{page.status}</h1>
+
+        <p class="mt-3.5 text-3xl font-semibold text-medium">
+            Yikes! {page.error?.message ?? "Something went awry."}
+        </p>
+
+        <p class="mt-3.5 text-m text-softened">
+            Please keep your seatbelt fastened while we guide you back to
+            safety.
+        </p>
+
+        <a
+            href="/"
+            class="inline-block mt-7 px-8 py-3 bg-medium text-lighter font-medium rounded-sm shadow hover:bg-softened transition"
+        >
+            Go Back Home
+        </a>
+    </div>
+</div>
