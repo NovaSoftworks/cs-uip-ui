21f46d4104a86a258dd8214230829d5483e54f08
Marc LAMY <29371248+marclamy@users.noreply.github.com>
2025-07-05 12:45:56 +0200

    add app layout

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,7 +1,38 @@
 <script lang="ts">
-	import '../../app.css';
-	
-	let { children } = $props();
+    import Sidebar from "$lib/components/sidebar.svelte";
+    import Header from "$lib/components/header.svelte";
+    import Footer from "$lib/components/footer.svelte";
+
+    import "../../app.css";
+
+    let { children } = $props();
+
+    // SIDEBAR
+    let sidebarOpen = $state(false);
+
+    function toggleSidebar() {
+        sidebarOpen = !sidebarOpen;
+    }
+
+    function closeSidebar() {
+        sidebarOpen = false;
+    }
 </script>
 
-{@render children()}
+<div
+    class="min-h-screen grid grid-rows-[auto_1fr_auto] md:grid-cols-[auto_1fr] bg-light text-darker"
+>
+    <Sidebar {sidebarOpen} {closeSidebar} />
+
+    <header class="row-start-1 md:col-start-2 p-7">
+        <Header {toggleSidebar} />
+    </header>
+
+    <main class="row-start-2 md:col-start-2 p-7">
+        {@render children()}
+    </main>
+
+    <footer class="row-start-3 md:col-start-2 pb-7">
+        <Footer />
+    </footer>
+</div>
