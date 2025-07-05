44c1172aceb748fbf5bef31ee51a23c0192e4b9c
Marc LAMY <29371248+marclamy@users.noreply.github.com>
2025-07-06 01:43:14 +0200

    fix Cookie name for Kratos

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,11 +1,12 @@
 import { redirect } from '@sveltejs/kit';
 
 const DEBUG_MODE = false;
+const COOKIE_NAME = 'ory_kratos_session';
 
 export function load({ cookies, url }) {
   if (!DEBUG_MODE) {
-    if (cookies.get('session')) {
-      console.log('Session cookie found:', cookies.get('session'));
+    if (cookies.get(COOKIE_NAME)) {
+      console.log('Session cookie found:', cookies.get(COOKIE_NAME));
     } else {
       console.log('No session cookie found, redirecting to /login');
       redirect(303, `/login?redirectTo=${url.pathname}`);
