d723ba9d3cec8d87b1754da0faedef9b8cce16f8
Marc LAMY <29371248+marclamy@users.noreply.github.com>
2025-07-05 12:43:00 +0200

    add session middleware

---
+++ src/routes/(app)/+layout.server.ts
@@ -0,0 +1,16 @@
+import { redirect } from '@sveltejs/kit';
+
+const DEBUG_MODE = false;
+
+export function load({ cookies, url }) {
+    if (!DEBUG_MODE) {
+        if (cookies.get('session')) {
+            console.log('Session cookie found:', cookies.get('session'));
+        } else {
+            console.log('No session cookie found, redirecting to /login');
+            redirect(303, `/login?redirectTo=${url.pathname}`)
+        }
+    } else {
+        console.log('DEBUG_MODE is enabled, skipping session check');
+    }
+}
