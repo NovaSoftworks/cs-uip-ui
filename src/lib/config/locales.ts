884513e23695d6904de2d4d2262a591736d9ca93
Marc LAMY <29371248+marclamy@users.noreply.github.com>
2025-07-07 19:51:08 +0200

    create locales config array

---
+++ src/lib/config/locales.ts
@@ -0,0 +1,9 @@
+export interface Locale {
+  locale: string;
+  name: string;
+}
+
+export const locales: Locale[] = [
+  { locale: 'en', name: 'English' },
+  { locale: 'fr', name: 'Français' }
+];
